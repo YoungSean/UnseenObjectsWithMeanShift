d13a0d599f32015d0760fc80a2afee72d23391c4
Yu Xiang <yu.xiang@utdallas.edu>
2023-01-15 16:33:29 -0600

    test checkpoints

--- experiments/scripts/demo_msmformer_rgb.sh
+++ experiments/scripts/demo_msmformer_rgb.sh
@@ -11,8 +11,8 @@ export CUDA_VISIBLE_DEVICES=0
 --color *-color.png   \
 --depth *-depth.png \
 --cfg experiments/cfgs/seg_resnet34_8s_embedding_cosine_color_tabletop.yml \
---pretrained data/checkpoints/RGB_norm_model_0069999.pth \
---pretrained_crop data/checkpoints/RGB_crop_model_final.pth \
+--pretrained data/checkpoints/rgb_pretrain/norm_RGB_pretrained.pth \
+--pretrained_crop data/checkpoints/rgb_pretrain/crop_RGB_pretrained.pth \
 --network_cfg MSMFormer/configs/tabletop_pretrained.yaml \
 --network_crop_cfg MSMFormer/configs/crop_tabletop_pretrained.yaml  \
 --input_image COLOR
