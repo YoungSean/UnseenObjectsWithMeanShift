48b6d26288984d396b1499c2dea17d63298ec5ae
Yu Xiang <yu.xiang@utdallas.edu>
2023-01-15 19:27:35 -0600

    fix script paths

--- experiments/scripts/demo_msmformer_rgbd.sh
+++ experiments/scripts/demo_msmformer_rgbd.sh
@@ -7,8 +7,8 @@ export CUDA_VISIBLE_DEVICES=0
 
 ./tools/test_image_with_ms_transformer.py  \
 --imgdir data/demo   \
---color color-*.jpg   \
---depth depth-*.png \
+--color *-color.png   \
+--depth *-depth.png \
 --cfg experiments/cfgs/seg_resnet34_8s_embedding_cosine_rgbd_add_tabletop.yml \
 --pretrained data/checkpoints/rgbd_pretrain/norm_RGBD_pretrained.pth \
 --pretrained_crop data/checkpoints/rgbd_pretrain/crop_RGBD_pretrained.pth \
--- experiments/scripts/demo_msmformer_rgbd_finetuned.sh
+++ experiments/scripts/demo_msmformer_rgbd_finetuned.sh
@@ -10,8 +10,8 @@ export CUDA_VISIBLE_DEVICES=0
 --color *-color.png   \
 --depth *-depth.png \
 --cfg experiments/cfgs/seg_resnet34_8s_embedding_cosine_rgbd_add_tabletop.yml \
---pretrained data/checkpoints/norm_RGBD_finetuned_data04_OCID_5epoch.pth \
---pretrained_crop data/checkpoints/crop_RGBD_finetuned_data04_OSD_1epoch.pth \
+--pretrained data/checkpoints/rgbd_finetuned/norm_RGBD_finetuned_data04_OCID_5epoch.pth \
+--pretrained_crop data/checkpoints/rgbd_finetuned/crop_RGBD_finetuned_data04_OSD_1epoch.pth \
 --network_cfg MSMFormer/configs/mixture_UCN.yaml \
 --network_crop_cfg MSMFormer/configs/crop_mixture_UCN.yaml \
 --input_image RGBD_ADD
