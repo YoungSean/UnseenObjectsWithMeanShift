af343160d6848b5a42155b576a76ae080a08e2ab
Yu Xiang <yu.xiang@utdallas.edu>
2023-01-18 11:36:47 -0600

    change checkpoint for ros

--- experiments/scripts/ros_seg_transformer_test_segmentation_fetch.sh
+++ experiments/scripts/ros_seg_transformer_test_segmentation_fetch.sh
@@ -10,10 +10,10 @@ outdir="data/checkpoints"
 
 ./ros/test_images_segmentation_transformer.py --gpu $1 \
 --cfg experiments/cfgs/seg_resnet34_8s_embedding_cosine_rgbd_add_tabletop.yml \
---pretrained data/checkpoints/norm_model_0069999.pth \
---pretrained_crop data/checkpoints/crop_dec9_model_final.pth \
---network_cfg MSMFormer/configs/tabletop_pretrained.yaml \
---network_crop_cfg MSMFormer/configs/crop_tabletop_pretrained.yaml \
+--pretrained data/checkpoints/rgbd_pretrain/norm_RGBD_pretrained.pth \
+--pretrained_crop data/checkpoints/rgbd_pretrain/crop_RGBD_pretrained.pth \
+--network_cfg MSMFormer/configs/mixture_UCN.yaml \
+--network_crop_cfg MSMFormer/configs/crop_mixture_UCN.yaml \
 --input_image RGBD_ADD \
 --camera Fetch \
 #--no_refinement     # comment this out if you want to use labe refinement
--- experiments/scripts/ros_seg_transformer_test_segmentation_fetch_finetune.sh
+++ experiments/scripts/ros_seg_transformer_test_segmentation_fetch_finetune.sh
@@ -10,8 +10,8 @@ outdir="data/checkpoints"
 
 ./ros/test_images_segmentation_transformer.py --gpu $1 \
 --cfg experiments/cfgs/seg_resnet34_8s_embedding_cosine_rgbd_add_tabletop.yml \
---pretrained data/checkpoints/model_rgbd_finetuned_0000799.pth \
---pretrained_crop data/checkpoints/crop_dec9_model_final.pth \
+--pretrained data/checkpoints/rgbd_finetuned/norm_RGBD_finetuned_data04_OCID_5epoch.pth \
+--pretrained_crop data/checkpoints/rgbd_finetuned/crop_RGBD_finetuned_data04_OSD_1epoch.pth \
 --network_cfg MSMFormer/configs/mixture_UCN.yaml \
 --network_crop_cfg MSMFormer/configs/crop_mixture_UCN.yaml \
 --input_image RGBD_ADD \
